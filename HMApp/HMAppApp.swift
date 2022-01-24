e439f1e8028c8865067d17f45df0001bb2a45559
DoS <doszlmnv@gmail.com>
2022-01-24 17:36:57 +0500

    Initial Commit

---
+++ HMApp/HMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMAppApp.swift
+//  HMApp
+//
+//  Created by Dossymkhan Zhulamanov on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HMApp/UIColor+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Ext.swift
+//  HMApp
+//
+//  Created by Dossymkhan Zhulamanov on 15.01.2022.
+//
+
+import SwiftUI
+
+extension Color{
+    
+    static func rgb(red:CGFloat, green: CGFloat, blue: CGFloat) -> Color{
+        return Color(red: red/255, green: green/255, blue: blue/255)
+    }
+    
+    static let mainDarkBlueBackgroundColor = Color(red: 1/255, green: 25/255, blue: 32/255)
+    
+    static let cardBackgroundColor = Color(red: 1/255, green: 35/255, blue: 45/255)
+    
+    static let fieldsMainGreenColor = Color(red: 5/255, green: 205/255, blue: 165/255)
+    
+}
+
